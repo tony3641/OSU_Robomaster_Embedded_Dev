fc85bb5ace7db9440b244d3c18a6a70cb5592b6b
Tony3641 <tony3641@126.com>
2019-05-27 20:01:48 -0400

    optimize aiming assistant performance
    Get parameters tuned

---
+++ OSU_RoboMaster_Embeded_Infantry_#2/standard/Project/Objects/mpu6500driver.d
@@ -0,0 +1,5 @@
+.\objects\mpu6500driver.o: ..\user\AHRS\mpu6500driver.c
+.\objects\mpu6500driver.o: ..\user\AHRS\mpu6500driver.h
+.\objects\mpu6500driver.o: ..\User\main.h
+.\objects\mpu6500driver.o: ..\user\AHRS\mpu6500driver_middleware.h
+.\objects\mpu6500driver.o: ..\user\AHRS\mpu6500reg.h
