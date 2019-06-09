bd699b87f68c7830fe062b3a924cc7a665b87ce1
lcpdeb <47087196+lcpdeb@users.noreply.github.com>
2019-06-09 08:51:41 +0800

    gimbal_motor->motor_gyro*0.2);

--- OSU_RoboMaster_Embeded_Infantry_#2/standard/user/APP/gimbal_task/gimbal_task.c
+++ OSU_RoboMaster_Embeded_Infantry_#2/standard/user/APP/gimbal_task/gimbal_task.c
@@ -817,7 +817,7 @@ static void gimbal_motor_aim_control_gyro_yaw(Gimbal_Motor_t *gimbal_motor)
 /*******************************************************************/
     if(abs((int)delta_yaw)>15)
 			{
-				gimbal_motor->motor_gyro_set = GIMBAL_PID_Calc(&gimbal_motor->gimbal_motor_absolute_angle_pid, gimbal_motor->absolute_angle, final_absolute_yaw_angle_set, gimbal_motor->motor_gyro*0.05);
+				gimbal_motor->motor_gyro_set = GIMBAL_PID_Calc(&gimbal_motor->gimbal_motor_absolute_angle_pid, gimbal_motor->absolute_angle, final_absolute_yaw_angle_set, gimbal_motor->motor_gyro*0.2);
 			}
 		else
 			{//If the pixel error is small(close to target)
